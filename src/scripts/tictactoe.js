f803b2766352957909582f053c369aa9753ad0c5
Jeshu Brij <jeshu911@gmail.com>
2021-02-02 00:37:18 +0530

    WIP

--- src/scripts/tictactoe.js
+++ src/scripts/tictactoe.js
@@ -3,35 +3,53 @@ import '../styles/tictactoe.scss';
 let isPlayerX = true;
 const players = document.querySelectorAll('.playerNames');
 const cells = document.querySelectorAll('.gameCells');
+const winingCombinations = [
+  '7',
+  '70',
+  '700',
+  '111',
+  '222',
+  '444',
+  '124',
+  '421',
+];
 
 function checkGameStatus() {
-  let xVals = '', oVals = '';
-  cells.forEach(cell =>{
-    if(cell.classList.contains('cross')) {
+  let xVals = '',
+    oVals = '';
+  cells.forEach((cell) => {
+    if (cell.classList.contains('cross')) {
       xVals += 1;
     } else {
       xVals += 0;
     }
-    if(cell.classList.contains('circle')) {
+    if (cell.classList.contains('circle')) {
       oVals += 1;
     } else {
       oVals += 0;
     }
-  })
-
-  console.log('X', xVals);
-  console.log('0', oVals);
+  });
+  console.log(xVals, oVals);
+  xVals = parseInt(xVals, 2).toString(8);
+  oVals = parseInt(oVals, 2).toString(8);
+  console.log(xVals, oVals);
+  if (winingCombinations.find((val) => val === xVals)) {
+    console.log('X wins....');
+  }
+  if (winingCombinations.find((val) => val === oVals)) {
+    console.log('O wins....');
+  }
 }
 
 function updatePlayerStatus() {
-  const active = document.querySelector('.playerNames.active')
-  players.forEach(player =>{
-    if(player === active) {
-      player.classList.remove('active')
+  const active = document.querySelector('.playerNames.active');
+  players.forEach((player) => {
+    if (player === active) {
+      player.classList.remove('active');
     } else {
-      player.classList.add('active')
+      player.classList.add('active');
     }
-  })
+  });
 }
 
 function playerTurn(evt) {
@@ -40,22 +58,22 @@ function playerTurn(evt) {
     evt.target.classList.add(isPlayerX ? 'cross' : 'circle');
   }
   checkGameStatus();
-  updatePlayerStatus()
+  updatePlayerStatus();
   isPlayerX = !isPlayerX;
 }
 
 function reset() {
-  cells.forEach(cell =>{
-    cell.classList.remove('cross')
-    cell.classList.remove('circle')
+  cells.forEach((cell) => {
+    cell.classList.remove('cross');
+    cell.classList.remove('circle');
   });
-  players.forEach((player, index) =>{
-    if(index === 0) {
+  players.forEach((player, index) => {
+    if (index === 0) {
       player.classList.add('active');
-    } else{
+    } else {
       player.classList.remove('active');
     }
-  })
+  });
 }
 
 function Tictactoe() {
